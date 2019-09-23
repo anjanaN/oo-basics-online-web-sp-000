ff3c69d57e9e507bf91817b4a71e711926a9ff12
Anjana Nair <anju740@gmail.com>
2019-09-23 02:59:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -13,24 +13,12 @@ class Book
     @author = author
   end
 
-  def author
-    @author
-  end
-
   def page_count=(num)
     @page_count = num
   end
 
-  def page_count
-    @page_count
-  end
-
   def genre=(genre)
     @genre = genre
-  end
-
-  def genre
-    @genre
   end
 
   def  turn_page
